499d81b3ad2dd5bf3be8fac33e7d876bf282dd28
Valentin Berger <Valentin.berger38@gmail.com>
2019-06-02 09:53:35 +0200

    :sparkles: JWT in sensible pages
    * The JWT is now checked during the reservation. If it has expired, the user can now longer access the `Reservation` page.

--- ClientApplication/src/main/java/com/champix/clientchampix/controller/ReservationController.java
+++ ClientApplication/src/main/java/com/champix/clientchampix/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.champix.clientchampix.controller;
 
 import com.champix.clientchampix.dto.ReservationDTO;
 import com.champix.clientchampix.jms.MessageJms;
+import com.champix.clientchampix.jwt.JWTManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +25,9 @@ public class ReservationController {
     @RequestMapping(method = RequestMethod.GET, value = "/reservation")
     public ModelAndView getReservation(HttpServletRequest request,
                                      HttpServletResponse response) throws Exception {
-
+        if (!checkJWTSession(request))
+            return new ModelAndView("views/error");
+        
         String destinationPage="";
         try {
             request.setAttribute("idVehicule", request.getParameter("idVehicule"));
@@ -39,6 +42,8 @@ public class ReservationController {
     @RequestMapping(method = RequestMethod.GET, value = "/envoiReservation")
     public ModelAndView envoiReservation(HttpServletRequest request,
                                      HttpServletResponse response) throws Exception {
+        if (!checkJWTSession(request))
+            return new ModelAndView("views/error");
 
         String destinationPage="";
         try {
@@ -66,5 +71,14 @@ public class ReservationController {
             destinationPage = "views/error";
         }
         return new ModelAndView(destinationPage);
+    }
+    
+    private boolean checkJWTSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (!JWTManager.verify(session.getAttribute("jwt").toString())) {
+            request.setAttribute("error", "Session expired");
+            return false;
+        }
+        return true;
     }
 }
